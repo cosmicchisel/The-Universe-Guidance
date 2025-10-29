30094a1f83bdd5456c8719cdc37c1ab554d38d48
Cosmicchisel <cosmicchisel1111@gmail.com>
2025-10-29 23:51:06 +0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,13 @@ import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './src/app.component';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { GeminiService } from './src/services/gemini.service';
+import { VoiceAssistantService } from './src/services/voice-assistant.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
+    GeminiService,
+    VoiceAssistantService,
   ],
 }).catch((err) => console.error(err));
